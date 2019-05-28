027d19dd24532c4d02d2bdef5ea9db286ebf42a7
Henelpj <37398182+Henelpj@users.noreply.github.com>
2019-05-28 11:44:37 +0530

    Update sample.php

--- sample.php
+++ sample.php
@@ -1,7 +1,12 @@
-<?php
 <html>
-  <body>
- <h1> Hiiii Henel </h1>
-    <body>
+<body>
+
+<?php
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?> 
+
+</body>
 </html>
-?>
